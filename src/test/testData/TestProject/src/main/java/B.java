982294e5d2cbd6c557896f77f240f53eba71152a
Bavindu <bavindubimsara.16@cse.mrt.ac.lk>
2021-01-30 15:25:26 +0530

    TestProgectis added to testData

---
+++ src/test/testData/TestProject/src/main/java/B.java
@@ -0,0 +1,18 @@
+public class B {
+    private int b = 5;
+    private A a ;
+    public B() {
+        a= new A("name");
+        a.setdsdName("dsds");
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+
+}
